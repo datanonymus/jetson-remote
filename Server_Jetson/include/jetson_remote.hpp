7cc027eb4814ac136223f37c80a105ca9f3f2d0c
datanonymus <dat625at1@gmail.com>
2026-08-30 22:52:00 +0700

    Test AES-128-CTR function for encrypt data - v2

--- Server_Jetson/include/jetson_remote.hpp
+++ Server_Jetson/include/jetson_remote.hpp
@@ -48,8 +48,8 @@ namespace JetsonRemote {
     extern std::string pending_device_id; // Nhớ lại xem thiết bị lạ nào đang xin cấp quyền
 
     // Khóa tĩnh 16-byte
-    extern const unsigned char AES_KEY[16]; 
-    extern const unsigned char AES_IV[16];
+    extern const unsigned char AES_KEY[20]; 
+    extern const unsigned char AES_IV[20];
 
     // Khai báo các hàm sẽ dùng
     void init_virtual_mouse(int width, int height);
